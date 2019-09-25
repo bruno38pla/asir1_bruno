8a1d8743a1e72270aed9f2e96a4de4ee6cc988cd
Bruno38pla <soybrunolamadrid@gmail.com>
2019-09-25 10:44:49 +0200

    version inicial

--- asir1bruno.php
+++ asir1bruno.php
@@ -1,5 +1,5 @@
 
-
+<a href="http://192.168.0.99/asir1_mario/asir1_1.php">MARIO</a>
 <p>Bruno</p>
 <a href="http://192.168.0.85/asir1_alex/asir1alex.php">Pagina de ALEX</a>
 
@@ -9,6 +9,7 @@
 
 <?php
 $f=fopen('visitas.txt','a');
+fwrite ($f, date('Y-m-d H:i '));
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
 fclose($f)
 ?>
---
+++ catalogo.php
@@ -0,0 +1,40 @@
+
+<h1> GITHUB </h1>
+
+<a href="https://github.com/AaronASIR/Asir1_aaron" target="_blank">AARON</a>
+<br> </br>
+<a href="https://github.com/Real6/ASIR1_Ruben" target="_blank">RUBEN</a>
+<br> </br>
+<a href="https://github.com/victor-zayas" target="_blank">victor-zayas</a>
+<br> </br>
+<a href="https://github.com/deivisg98" target="_blank">David</a>
+<br> </br>
+<a href=":https://github.com/pablomoreta/asir1_pablo.git" target="_blank">Pablo moreta</a>
+<br> </br>
+<a href="https://github.com/adrivg19/asir1_adrian" target="_blank">Avillar</a>
+<br> </br>
+<a href="https://github.com/bruno38pla/ASIR1_BRUNO" target="_blank">Bruno</a>
+<br> </br>
+<a href="https://github.com/MarioAlonso31info/asir1_mario" target="_blank">Mario</a>
+<br> </br>
+<a href="https://github.com/AlejandroGoVi/asir1_alex" target="_blank">Alejandro</a>
+<br> </br>
+<a href="https://github.com/carlosccsan/asir1-cc" target="_blank">Carlos</a>
+<br> </br>
+<a href="https://github.com/gargiljavi" target="_blank">Javi</a>
+<br> </br>
+<a href="https://github.com/sergiosamperio/asir1_sergio" target="_blank">Sergio</a>
+<br> </br>
+<a href="https://github.com/draseld/asir1_luis" target="_blank">Luis</a>
+<br> </br>
+<a href="https://github.com/Saioagil" target="_blank">Saioa</a>
+<br> </br>
+<a href="https://github.com/alejandro-vgr/asir176" target="_blank">Ale</a>
+<br> </br>
+<a href="https://github.com/serran24/asir1_victor" target="_blank">Victor Serrano</a>
+<br> </br>
+<a href="https://github.com/kevin-raico-97" target="_blank">Kevin Roldan</a>
+<br> </br>
+<a href="https://github.com/rolandoMH96/Asir1-RMH" target="_blank">Rolando</a>
+<br> </br>
+<a href="https://github.com/dblancoformacion/" target="_blank">DAVID BLANCO</a>
