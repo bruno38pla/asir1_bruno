584c8c422db7c405cce15928fb2375c39c5e5024
Bruno38pla <soybrunolamadrid@gmail.com>
2019-11-20 10:14:57 +0100

    version inicial

---
+++ Unidad 2 Backend/html/html.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Zona de arte</title>
+</head>
+<center> <b> <font size="60" color="blue">Napoleon Bonaparte</font> </b> </center>
+<center>Asi de centrado quiero estar yo</center>
+ <body bgcolor="#FFA07A" topmargin="100" leftmargin="0" marginheight="0" marginwidth="50">
+<img src="napoles.jpg" alt="Napolonico" height="420" width="420 ">  <img src="napoleon.jpg" align="right" height="420" width="420 ">
+<p><a href="https://www.nationalgeographic.com.es/personajes/napoleon">Napoleon Bonaparte</a></p>
+<h1><u><center>- EMPERADOR -</h1></u></center>
+<b>La figura de Napoleón Bonaparte emergió durante la Revolución Francesa, 
+y con el paso de los años se agigantó hasta convertirse en uno de los símbolos militares y políticos más importantes de la historia de Europa. 
+Como emperador, dominó su país y lo embarcó en una espiral expansionista: las Guerras Napoleónicas. 
+<br></b>
+<br>
+</br>
+<img src="francia.jpg" alt="Napolonico" height="420" width="520 "> <img src="batallas.jpg" align="right" height="420" width="620 ">
+<p><a href="https://www.ecured.cu/Guerras_napole%C3%B3nicas">Guerras Napoleonicas</a></p>
+<h1><u><center>- GUERRAS NAPOLEONICAS -</h1></u></center>
+<b> Fueron una serie de conflictos bélicos que tuvieron lugar durante el tiempo en que el emperador Napoleón I Bonaparte gobernó en Francia.
+ Fueron en parte una extensión de los conflictos que estallaron a causa de la Revolución francesa y continuaron, 
+ a instigación y gracias al financiamiento del Reino Unido, durante todo el Primer Imperio francés. </b>
+<br>
+</br>
+ <center> <table border="3" >
+  <tr>
+    <th>Napoleon I</th>
+  </tr>
+  <tr>
+    <td>Emperador de Francia</td>
+  </tr>
+  <tr>
+    <td>Coprincipe de Andorra</td>
+  </tr>
+    <tr>
+    <td>Rey de Italia</td>
+  </tr>
+  <tr>
+   <td>Protector de la confederacion del Rin</td>
+    </tr>
+</table> </center>
+</body>
+</html>
