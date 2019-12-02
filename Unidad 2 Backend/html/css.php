6c53f6219ea65427fa5ece516a24d798aefb0538
Bruno38pla <soybrunolamadrid@gmail.com>
2019-12-02 13:06:23 +0100

    HTML v3

---
+++ Unidad 2 Backend/html/css.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<title>Zona de arte</title>
+</head>
+ <body bgcolor="#FFA07A" topmargin="100" leftmargin="0" marginheight="0" marginwidth="50">
+<center> <b> <font size="60" color="blue">Napoleon Bonaparte</font> </b> </center>
+<center>Asi de centrado quiero estar yo</center>
+<img src="napoles.jpg" alt="Napolonico" height="420" width="420 ">  <img src="napoleon.jpg" align="right" height="420" width="420 ">
+<p><a href="https://www.nationalgeographic.com.es/personajes/napoleon">Napoleon Bonaparte</a></p>
+<h1><u><center>- EMPERADOR -</h1></u></center>
+<b>La figura de Napoleón Bonaparte emergió durante la Revolución Francesa, 
+y con el paso de los años se agigantó hasta convertirse en uno de los símbolos militares y políticos más importantes de la historia de Europa. 
+Como emperador, dominó su país y lo embarcó en una espiral expansionista: las Guerras Napoleónicas. 
+<br></br>
+<br>
+</br>
+<img src="francia.jpg" alt="Napolonico" height="420" width="520 "> <img src="batallas.jpg" align="right" height="420" width="620 ">
+<p><a href="https://www.ecured.cu/Guerras_napole%C3%B3nicas">Guerras Napoleonicas</a></p>
+<h2><u><center>- GUERRAS NAPOLEONICAS -</h2></u></center>
+<b> Fueron una serie de conflictos bélicos que tuvieron lugar durante el tiempo en que el emperador Napoleón I Bonaparte gobernó en Francia.
+ Fueron en parte una extensión de los conflictos que estallaron a causa de la Revolución francesa y continuaron, 
+ a instigación y gracias al financiamiento del Reino Unido, durante todo el Primer Imperio francés. </b>
+<br>
+<h3><u><center>- GUERRAS -</h3></u></center>
+</br>
+ <center> <table border="3" >
+  <tr>
+    <th>Napoleon I</th>
+  </tr>
+  <tr>
+    <td>Emperador de Francia</td>
+  </tr>
+  <tr>
+    <td>Coprincipe de Andorra</td>
+  </tr>
+    <tr>
+    <td>Rey de Italia</td>
+  </tr>
+  <tr>
+   <td>Protector de la confederacion del Rin</td>
+    </tr>
+</table> </center>
+<br>
+</br>
+<h4><u><center>- GUERRAS -</h4></u></center>
+ <center> <table border="1" >
+  <tr>
+    <th>Provincias bañadas por el cantabrico</th>
+  </tr>
+  <tr>
+    <td>Pais Vasco</td>
+  </tr>
+  <tr>
+    <td>Cantabria</td>
+  </tr>
+    <tr>
+    <td>Asturias</td>
+  </tr>
+</table> </center>
+<br>
+</br>
+  <form>
+  Nombre:<br/>
+  <input type="text" name="firstname"><br/>
+  Apellidos:<br/>
+  <input type="text" name="lastname"/>
+  <input type="submit" value="Submit"/>
+</body>
+</html>
