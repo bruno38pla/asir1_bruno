cfcf1112135cd05ebd5ff60fc7344c01a3a55d36
Bruno38pla <soybrunolamadrid@gmail.com>
2019-12-02 14:50:16 +0100

    HTML v3

--- Unidad 2 Backend/html/css.php
+++ Unidad 2 Backend/html/css.php
@@ -7,7 +7,7 @@
 <center>Asi de centrado quiero estar yo</center>
 <img src="napoles.jpg" alt="Napolonico" height="420" width="420 ">  <img src="napoleon.jpg" align="right" height="420" width="420 ">
 <p><a href="https://www.nationalgeographic.com.es/personajes/napoleon">Napoleon Bonaparte</a></p>
-<h1><u><center>- EMPERADOR -<h1/></u><center/>
+<h1><u><center>- EMPERADOR -<h1/><u/><center/>
 <b>La figura de Napoleón Bonaparte emergió durante la Revolución Francesa, 
 y con el paso de los años se agigantó hasta convertirse en uno de los símbolos militares y políticos más importantes de la historia de Europa. 
 Como emperador, dominó su país y lo embarcó en una espiral expansionista: las Guerras Napoleónicas. 
@@ -16,7 +16,7 @@ Como emperador, dominó su país y lo embarcó en una espiral expansionista: las
 </br>
 <img src="francia.jpg" alt="Napolonico" height="420" width="520 "> <img src="batallas.jpg" align="right" height="420" width="620 ">
 <p><a href="https://www.ecured.cu/Guerras_napole%C3%B3nicas">Guerras Napoleonicas</a></p>
-<h2><u><center>- GUERRAS NAPOLEONICAS -</h2></u></center>
+<h2><u><center>- GUERRAS NAPOLEONICAS -<h2/><u/><center/>
 <b> Fueron una serie de conflictos bélicos que tuvieron lugar durante el tiempo en que el emperador Napoleón I Bonaparte gobernó en Francia.
  Fueron en parte una extensión de los conflictos que estallaron a causa de la Revolución francesa y continuaron, 
  a instigación y gracias al financiamiento del Reino Unido, durante todo el Primer Imperio francés. </b>
@@ -42,7 +42,7 @@ Como emperador, dominó su país y lo embarcó en una espiral expansionista: las
 </table> </center>
 <br>
 </br>
-<h4><u><center>- GUERRAS -<h4/></u><center/>
+<h4><u><center>- GUERRAS -<h4/><u/><center/>
  <center> <table border="1" >
   <tr>
     <th>Provincias bañadas por el cantabrico</th>
